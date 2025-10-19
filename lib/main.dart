aed179b18716551041fe708d13844e75f58567be
Giang0402 <giangb2205976@student.ctu.edu.vn>
2025-10-19 19:50:11 +0700

    manage supabase anon key with .env

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart'; // Import package
 import 'app.dart';
 
-// App's main entry point.
 Future<void> main() async {
-  // Ensure Flutter bindings are initialized before running the app.
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Initialize the Supabase client.
-  // TODO: Replace with your project's URL and anon key from the Supabase dashboard.
+  // Load environment variables from the .env file
+  await dotenv.load(fileName: ".env");
+
+  final url = dotenv.env['SUPABASE_URL'];
+  final anonKey = dotenv.env['SUPABASE_ANON_KEY'];
+
+  // Initialize Supabase 
   await Supabase.initialize(
-    url: 'https://ezdqwuypgldtkcwtuate.supabase.co',
-    anonKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV6ZHF3dXlwZ2xkdGtjd3R1YXRlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk5NDYxNjEsImV4cCI6MjA3NTUyMjE2MX0.ATd8pP2zRvw91yWeK4SqeTmsPvCYdoSZktQzhta2x2s',
+    url: url!,
+    anonKey: anonKey!,
   );
 
-  // Run the app within a ProviderScope for Riverpod state management.
   runApp(const ProviderScope(child: App()));
 }
